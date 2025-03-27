82590fe965ddd43f7a9bff77bf615d5015a21ff2
David Smejkal <david@smejkal.software>
2025-03-27 01:27:02 +0000

    v0.7.0-beta implemented multiprocessing via -j (--jobs) parameter

--- tests/functional/test_wikimedia.py
+++ tests/functional/test_wikimedia.py
@@ -7,7 +7,8 @@ from io import BytesIO
 @pytest.mark.parametrize(
     "input_file, expected_output, redirects_file, expected_redirects_output, "
     + "links_file, expected_links_output, "
-    + "categories_file, expected_categories_output",
+    + "categories_file, expected_categories_output, "
+    + "jobs",  # Added jobs parameter
     [
         ### TEST #1 simple input output test
         (  # cat tests/data/52-pages-wikimedia.xml | python wiki2txt.py > tests/data/52p-txt.xml
@@ -18,7 +19,8 @@ from io import BytesIO
             None,
             None,
             None,
-            None,  # default behaviour
+            None,
+            1,  # default behaviour, single-threaded
         ),
         ### TEST #2 parsing out redirects
         (  # cat tests/data/52-pages-wikimedia.xml | python wiki2txt.py -r tests/data/52p-red.edg > tests/data/52p-txt-no-red.xml
@@ -30,6 +32,7 @@ from io import BytesIO
             None,
             None,
             None,
+            1,  # single-threaded
         ),
         ### TEST #3 parsing out links
         (  # cat tests/data/52-pages-wikimedia.xml | python wiki2txt.py -l tests/data/52p-lnk.edg > tests/data/52p-txt-no-lnk.xml
@@ -41,6 +44,7 @@ from io import BytesIO
             "tests/data/52p-lnk.edg",
             None,
             None,
+            1,  # single-threaded
         ),
         ### TEST #4 parsing out categories
         (  # cat tests/data/52-pages-wikimedia.xml | python wiki2txt.py -c tests/data/52p-cat.edg > tests/data/52p-txt-no-cat.xml
@@ -52,6 +56,7 @@ from io import BytesIO
             None,
             BytesIO(),
             "tests/data/52p-cat.edg",
+            1,  # single-threaded
         ),
         ### TEST #5 parsing out everything (redirects, links and categories)
         (  # cat tests/data/52-pages-wikimedia.xml | python wiki2txt.py -r tests/data/52p-red.edg -l tests/data/52p-lnk-no-red.edg -c tests/data/52p-cat-no-red.edg > tests/data/52p-txt-no-lnk-no-cat.xml
@@ -63,6 +68,19 @@ from io import BytesIO
             "tests/data/52p-lnk-no-red.edg",  # NOTICE: if redirects are being parsed out then those pages will be left out of links edg file as well
             BytesIO(),
             "tests/data/52p-cat-no-red.edg",  # NOTICE: if redirects are being parsed out then those pages will be left out of categories edg file as well
+            1,  # single-threaded
+        ),
+        ### TEST #6 multiprocessing with 2 jobs
+        (  # cat tests/data/52-pages-wikimedia.xml | python wiki2txt.py -j 2 > tests/data/52p-txt.xml
+            "tests/data/52-pages-wikimedia.xml",
+            "tests/data/52p-txt.xml",
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            2,  # multiprocessing with 2 jobs
         ),
     ],
 )
@@ -75,6 +93,7 @@ def test_wikimedia_parsing(
     expected_links_output,
     categories_file,
     expected_categories_output,
+    jobs,  # Added jobs parameter
 ):
     processor = wiki2txt.Processor()
     processor.get_options()
@@ -87,6 +106,7 @@ def test_wikimedia_parsing(
     processor.arg_redirects_file = redirects_file
     processor.arg_links_file = links_file
     processor.arg_categories_file = categories_file
+    processor.jobs = jobs  # Set number of jobs for multiprocessing
 
     processor.ParseWiki()  # start parsing
 
@@ -107,3 +127,6 @@ def test_wikimedia_parsing(
                 assert (
                     processor.arg_categories_file.getvalue() == e_c_o.read()
                 )  ## test categories output
+
+    # Clean up input file handle
+    processor.arg_input.close()  # Explicitly close since it’s always a file handle
